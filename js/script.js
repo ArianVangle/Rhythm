df5584e169533da1e3565151c34fee42626a6313
Arian <126694213+ArianVangle@users.noreply.github.com>
2023-08-08 18:56:38 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ if (reviewsSwiper) {
 	const swiper = new Swiper('.swiper-reviews', {
 		// Optional parameters
 		loop: true,
+		observer: true,
 		// If we need pagination
 		pagination: {
 			el: '.swiper-pagination',
